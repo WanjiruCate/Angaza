a702764519bbae6f92491984a338aa77dd5689c2
Wanjiru Catherine <catherinemunene802@gmail.com>
2019-07-29 21:52:35 +0300

    Splash screen completed

--- app/src/main/java/com/shirucodes/angaza/UserInterface/SplashScreen.java
+++ app/src/main/java/com/shirucodes/angaza/UserInterface/SplashScreen.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.shirucodes.angaza.R;
+
 public class SplashScreen extends AppCompatActivity {
 
     @Override
