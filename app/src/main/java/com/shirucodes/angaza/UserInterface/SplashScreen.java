a0bceca31b8e9691f26b18c3d33702fb280f4ca6
Wanjiru Catherine <catherinemunene802@gmail.com>
2019-07-29 23:17:29 +0300

    Added the two start screens UI

--- app/src/main/java/com/shirucodes/angaza/UserInterface/SplashScreen.java
+++ app/src/main/java/com/shirucodes/angaza/UserInterface/SplashScreen.java
@@ -2,7 +2,9 @@ package com.shirucodes.angaza.UserInterface;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.shirucodes.angaza.R;
 
@@ -12,5 +14,15 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(SplashScreen.this,StartScreen.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        }, 3000);
     }
 }
--- app/src/main/java/com/shirucodes/angaza/UserInterface/StartScreen.java
+++ app/src/main/java/com/shirucodes/angaza/UserInterface/StartScreen.java
@@ -2,7 +2,9 @@ package com.shirucodes.angaza.UserInterface;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.shirucodes.angaza.R;
 
@@ -12,5 +14,15 @@ public class StartScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_screen);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(StartScreen.this,StartScreen2.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        }, 2000);
     }
 }
