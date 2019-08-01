dea76725a4791c850a9ab28c79532ea561896c80
Injiri <simoninjiri@gmail.com>
2019-08-01 08:22:26 +0300

    resolved sqlite db crash

--- app/src/main/java/com/shirucodes/angaza/SplashView/SplashScreen.java
+++ app/src/main/java/com/shirucodes/angaza/SplashView/SplashScreen.java
@@ -3,10 +3,15 @@ package com.shirucodes.angaza.SplashView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 
 import com.shirucodes.angaza.R;
+import com.shirucodes.angaza.database.databasecontacts.DatabaseContract;
+import com.shirucodes.angaza.database.helpers.AngazaDatabaseOpenHelper;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -15,11 +20,13 @@ public class SplashScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-        new Handler().postDelayed(new Runnable(){
+        Log.e("Tag", "onCreate: " + DatabaseContract.SQL_CREATE_RECENT_VERIFICATION_TABLE);
+        new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 /* Create an Intent that will start the Menu-Activity. */
-                Intent mainIntent = new Intent(SplashScreen.this,StartScreen.class);
+                Intent mainIntent = new Intent(SplashScreen.this, StartScreen.class);
+
                 startActivity(mainIntent);
                 finish();
             }
